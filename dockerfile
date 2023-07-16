d2ba023588cdf96b5a9c0655fcabe7f41036b775
rafaelbacungan <rbb52775@gmail.com>
2023-07-16 22:29:36 +0800

    add sql files

--- dockerfile
+++ dockerfile
@@ -2,7 +2,7 @@ FROM golang
 RUN mkdir /app
 ADD . /app
 WORKDIR /app
-RUN go build -o main ./cmd/server/main.go
+RUN go build -o main .
 
 EXPOSE 8080
 CMD ["/app/main"]
---
+++ migrations/2_comments.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS comments (
+    id SERIAL PRIMARY KEY,
+    comment VARCHAR NOT NULL,
+    comment_date DATE DEFAULT CURRENT_DATE,
+    user_id BIGINT REFERENCES users(id)
+);
+
+INSERT INTO users(name) values('dev_test_user');
+INSERT INTO comments(comment) VALUES('first test comment');
